08558c043e2dcf2e70ac7234452d6da248e4188d
sky-big <sky_big@yeah.net>
2019-11-18 18:41:31 +0800

    add all runtime template

--- fscli/commands/init/__init__.py
+++ fscli/commands/init/__init__.py
@@ -7,8 +7,18 @@ import logging
 import click
 
 from fscli.cli.main import pass_context, common_options
+from fscli.lib.globals.runtimes import *
 
 LOG = logging.getLogger(__name__)
+
+RUNTIMES_TEMPLATES = {
+    RUNTIME_PYTHON27: "",
+    RUNTIME_PYTHON36: "",
+    RUNTIME_PYTHON37: "",
+    RUNTIME_PHP7: "",
+    RUNTIME_NODEJS6: "",
+    RUNTIME_NODEJS8: "",
+}
 
 @click.command(
     "init",
---
+++ fscli/lib/globals/runtimes.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# python2.7
+RUNTIME_PYTHON27 = "python2.7"
+
+# python3.6
+RUNTIME_PYTHON36 = "python3.6"
+
+# python3.7
+RUNTIME_PYTHON37 = "python3.7"
+
+# PHP7
+RUNTIME_PHP7 = "php7"
+
+# nodejs6
+RUNTIME_NODEJS6 = "nodejs6"
+
+# nodejs8
+RUNTIME_NODEJS8 = "nodejs8"
+
+# all support runtime
+RUNTIMES = {
+    RUNTIME_PYTHON27: "",
+    RUNTIME_PYTHON36: "",
+    RUNTIME_PYTHON37: "",
+    RUNTIME_PHP7: "",
+    RUNTIME_NODEJS6: "",
+    RUNTIME_NODEJS8: "",
+}
